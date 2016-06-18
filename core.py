8dd96f5384f618ad823fd2aabcdae6bf6995479d
in9b <asayuuya.m9@gmail.com>
2016-06-19 06:28:32 +0900

    コア部分を実装

---
+++ core.py
@@ -0,0 +1,34 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+#生存:0,死亡1
+class Lifegame():
+    def __init__(self,n):
+        self.field = [[1 for i in range(n)] for j in range(n)]
+    def forward(self):
+        self.nextField = [[1 for i in range(len(self.field))] for j in range(len(self.field))]
+        for a in range(len(self.field)):
+            for b in range(len(self.field)):
+                self.checkCell(a,b)
+        self.updateField()
+    def countSurvival(self,i,j): #生存数を教えてくれるやつ
+        count = 0
+        for a in range(-1,2):
+            for b in range(-1,2):
+                if (i + a > 0 and j + b > 0 and i + a < len(self.field) and j + b < len(self.field)): 
+                    if (self.field[i+a][j+b] == 0 and ((a != 0) or (b != 0))):
+                        count += 1
+        return count
+    def checkCell(self,i,j): #cellに対して生存死亡判定をしてくれるやつ
+        tmp = self.countSurvival(i,j)
+        if tmp == 3:
+            self.nextField[i][j] = 0
+        elif tmp >= 4 or tmp <= 1:
+            self.nextField[i][j] = 1
+        else:
+            self.nextField[i][j] = self.field[i][j]
+    def updateField(self):
+        self.field = self.nextField
+    def printField(self):
+        for a in range(len(self.field)):
+            print(self.field[a])
+
